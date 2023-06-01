c524053cf337e46f5cb3584f21292ddcd8758cd0
tvrcgo <tvrcgo@gmail.com>
2023-06-01 10:00:11 +0800

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -9,33 +9,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <h1>Vite + React</h1>
         <p>
           <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+            count: {count}
           </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
         </p>
       </header>
     </div>
